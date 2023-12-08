ba22ab3b73d70aa613fa1a2a3d625a74260fbefa
zsefvcx <zsefvcx@list.ru>
2023-12-08 12:02:41 +0300

    firmware esp8266 mod

--- ESP8266Project/sketch_ath_bmp280_DHT/sketch_ath_bmp280_DHT.ino
+++ ESP8266Project/sketch_ath_bmp280_DHT/sketch_ath_bmp280_DHT.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <WiFiUdp.h>
 
 #define BMP_SCK  (13)
 #define BMP_MISO (12)
@@ -10,6 +11,7 @@
 #define BMP_CS   (10)
 #define DHTPIN    D4
 #define DHTTYPE   DHT22 
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_AHTX0 aht;
 Adafruit_BMP280 bmp; // I2C
@@ -17,6 +19,23 @@ Adafruit_BMP280 bmp; // I2C
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
+
+#pragma pack(push, 1)
+struct Status{
+uint8_t key[3];
+bool alarm;
+float t;//-50 ... 100
+float h;//0 ... 100
+bool err; 
+float t2;//-50 ... 100
+float h2;//0 ... 100 
+bool err2;
+float p;//700 ... 800 
+float Altitude;
+int8_t src;
+} statusFW;
+#pragma pack(pop)
+
 
 void setup() {
   Serial.begin(115200);
@@ -64,6 +83,7 @@ void setup() {
 
 float tBmpInt=-255, tAthInt=-255, tDhtExt=-255; 
 float pBmpInt = -255;
+float aBmpInt = -255;
 float toMmHg = 0.00750063755419211;
 float hAthInt=-255, hDhtEct=-255;
 
@@ -72,11 +92,14 @@ void getBmpData(){
       // can now print out the new measurements
       tBmpInt = bmp.readTemperature();
       pBmpInt = (bmp.readPressure())*toMmHg;
+      aBmpInt =  bmp.readAltitude(SEALEVELPRESSURE_HPA);
       Serial.print(F("Temperature = ")); Serial.print(tBmpInt); Serial.println(" *C");
       Serial.print(F("Pressure = ")); Serial.print(pBmpInt); Serial.println(" mmHh");
     } else {
       Serial.println("Forced measurement failed!");
       tBmpInt=-255;
+      pBmpInt=-255;
+      aBmpInt=-255;
     }
 }
 
@@ -113,19 +136,54 @@ void getDhtData(){
 
 }
 
+byte getHash(byte* data, int length) {
+  byte hash = 0;
+  int i = 0;
+  while (length--) {
+    hash += *(data + i);
+    i++;
+  }
+  return hash;
+}
+
+void setStatusFW(){
+  statusFW.key[0]  = 0xAA;
+  statusFW.key[1]  = 0xAA;
+  statusFW.key[2]  = 0xAB;
+  statusFW.alarm   = tBmpInt==-255||tAthInt==-255||tDhtExt==-255||pBmpInt==-255 ||hAthInt==-255||hDhtEct==-255||aBmpInt==-255; 
+  statusFW.err     = tBmpInt==-255||tAthInt==-255||               pBmpInt==-255|| hAthInt==-255               ||aBmpInt==-255;
+  statusFW.err2    =                               tDhtExt==-255||                               hDhtEct==-255;
+  statusFW.t       = (tBmpInt+tAthInt)/2; 
+  statusFW.h       = hAthInt;
+  statusFW.p       = pBmpInt;
+  statusFW.Altitude= aBmpInt;
+  statusFW.t2      = tDhtExt;
+  statusFW.h2      = hDhtEct;
+  statusFW.src = getHash((byte*)&statusFW, sizeof(statusFW));
+}
+
+
 void loop() {
+  static int i  = 0; i++;  
   Serial.println(F("------------------------------------"));
+  Serial.print("Packet:"); Serial.println(i);
   Serial.println(F("-getBmpData-------------------------"));
   getBmpData();
   Serial.println(F("-getAhtData-------------------------"));
   getAhtData();
   Serial.println(F("-getDhtData-------------------------"));
   getDhtData();
+  Serial.println(F("-setStatusFW------------------------"));
+  setStatusFW();
+
   Serial.println(F("------------------------------------"));
   delay(2000);delay(delayMS);
-
-
   
 
-  
+
+  if (i >= 5){
+    Serial.println(F("-Sleep-600e6--------------------------"));
+    ESP.deepSleep(600e6); // сон  (10 минут = 600e6) или 0 - чтобы не просыпаться самостоятельно
+    i =  0;
+  }
 }
