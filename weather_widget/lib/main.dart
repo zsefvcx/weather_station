2b1913205b041bb853bfc0defecbba16066bab2a
zsefvcx <zsefvcx@list.ru>
2024-04-16 16:51:07 +0300

    add ui for windows 60% add ui for mobile 10% add widget for mobile 0% add ui settings 30% Ошибки ??? Стабильность повышена )) ++ разное

--- weather_widget/lib/main.dart
+++ weather_widget/lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -48,6 +50,19 @@ Future<void> main() async {
       await windowManager.show();
       await windowManager.focus();
     });
+
+
+    final window = await DesktopMultiWindow.createWindow(jsonEncode({
+      'args1': 'Sub window',
+      'args2': 100,
+      'args3': true,
+      'buissiness': 'bussiness_test',
+    }));
+    await window.setFrame(const Offset(0, 0) & const Size(1280, 720));
+    await window.center();
+    await window.setTitle('Another window');
+
+    await window.show();
   }
 
 
